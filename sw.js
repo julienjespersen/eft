8be23cf8efa24df8592ede326fe8ceb84d19c31c
Julien Jespersen <julien.jespersen@unige.ch>
2024-07-01 17:22:05 +0100

    inti

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
 // sw.js
+const web_cache = 'web-app-cache-v1.0';
+const filesToCache = [
+  '/',
+  'bulma.min.css',
+  'style.css',
+  '/img/app_icon.svg'
+];
+
+self.addEventListener('install',(event)=> {
+  event.waitUntil(
+    caches.open(web_cache)
+      .then((cache)=> {
+        //Cache has been opened successfully
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
